3c1d9d6cd5a8e0c6bf8cb54b0eb1e51622702987
Bilal <bilalbilin12@gmail.com>
2024-08-07 15:19:53 +0300

    Add project files.

---
+++ PatikaWeek4PracticeEncapsulation/Car.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaWeek4PracticeEncapsulation
+{
+    public class Car // Creating a new class
+    {
+        // Fields
+        private string _brand;
+        private string _model;
+        private string _colour;
+        private int _gateCount;
+
+        //Properties
+        public string Brand { get; set; }
+        
+        public string Model { get; set; }
+        
+        public string Colour { get; set; }
+
+        public int GateCount // Task instruction that encapsulates gate counting
+        { 
+            get 
+            { 
+                return _gateCount; 
+            } 
+            set 
+            {
+                if (value == 2 || value == 4) // Checking 2 or 4 situations
+                {
+                    _gateCount = value;
+                }
+                else // if not making gatecount -1
+                {
+                    Console.WriteLine("Kapı sayısı 2 veya 4 olabilir başka seçeneğimiz yoktur.");
+                    _gateCount = -1;
+                }
+            } 
+        }
+
+        public void CarInfo() // Method to show car informations
+        {
+            Console.WriteLine($"Marka --> {Brand}\nModel --> {Model}\nRenk --> {Colour}\nKapı Sayısı --> {GateCount}");
+        }
+
+    }
+}
---
+++ PatikaWeek4PracticeEncapsulation/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using PatikaWeek4PracticeEncapsulation;
+
+// Creating an instance of Car
+Car car1 = new Car();
+
+// Ask user to define properties
+Console.WriteLine("Lütfen bir Marka giriniz..");
+car1.Brand = Console.ReadLine();
+
+Console.WriteLine("Lütfen bir Model giriniz..");
+car1.Model = Console.ReadLine();
+
+Console.WriteLine("Lütfen bir renk giriniz..");
+car1.Colour = Console.ReadLine();
+
+Console.WriteLine("Lütfen kapı sayısını yazınız..");
+car1.GateCount = Convert.ToInt32(Console.ReadLine());
+
+// Display the car information by answer of user
+car1.CarInfo();
